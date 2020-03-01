4e751e787a1873cc863ed3152c193eb36c695b09
AvinashBarade <40133135+AvinashBarade@users.noreply.github.com>
2020-03-02 00:14:07 +0530

    New chnges

--- calculator.js
+++ calculator.js
@@ -3,14 +3,17 @@ function showresult(choise) {
     var l2 = parseFloat(document.getElementById('l2').value);
     var r1 = parseFloat(document.getElementById('r1').value);
     var r2 = parseFloat(document.getElementById('r2').value);
-
-    if (!l2 || !Number.isNaN(l2))
-        l2 = l1
-    if (!r2 || !Number.isNaN(r2) )
-        r2 = r1
-    var g = (((l1 + l2) / 2) * ((r1 + r2) / 2)) / 1089;
+    var length,width;
+    if (!l2)
+        length = l1;
+    else
+        length = (l1 + l2) / 2
+    if (!r2)
+        width = r1;
+    else
+        width = (r1 + r2) / 2
+    var g = (length * width) / 1089;
     var y = g / 40;
-
 
     document.getElementById('gunthe').value = g.toFixed(2);
     document.getElementById('yekar').value = y.toFixed(2);
